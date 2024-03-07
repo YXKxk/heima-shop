37cf47d462af4e36b48efd9fca1b336be2437d25
YXKxk <1843978049@qq.com>
2024-03-07 19:55:02 +0800

    猜你喜欢分页条件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,38 +1,63 @@
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
-import { onLoad } from '@dcloudio/uni-app'
-import { getHomeBannerApI, getHomeCategoryAPI } from '@/services/home'
-import type { BannerItem, CategoryItem } from '@/types/home'
-import { ref } from 'vue'
 import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
+import HotPanel from '@/pages/index/components/HotPanel.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import { getHomeBannerApI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import { ref } from 'vue'
+import type { XtxGuessInstance } from '@/types/component'
 
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerApI()
   bannerList.value = res.result
 }
-const list = ref<CategoryItem[]>([])
+const categoryList = ref<CategoryItem[]>([])
 
 //获取前台分类数据
 const getHomeCategoryData = async () => {
   const res = await getHomeCategoryAPI()
-  list.value = res.result
+  categoryList.value = res.result
+}
+
+const hotList = ref<HotItem[]>([])
+const getHomeHotPanel = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+// 获取猜你喜欢组件实例
+const guessRef = ref<XtxGuessInstance>()
+
+// 滚动触底事件
+const onScrolltolower = () => {
+  guessRef.value?.getMore()
 }
 onLoad(() => {
   getHomeBannerData()
   getHomeCategoryData()
+  getHomeHotPanel()
 })
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper :list="bannerList" />
-  <CategoryPanel :list="list" />
-  <view class="index">index </view>
+  <scroll-view scroll-y @scrolltolower="onScrolltolower">
+    <XtxSwiper :list="bannerList" />
+    <CategoryPanel :list="categoryList" />
+    <HotPanel :list="hotList" />
+    <XtxGuess ref="guessRef" />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,6 @@
 import { http } from '@/utils/http'
-import type { BannerItem, CategoryItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem, GuessItem } from '@/types/home'
+import type { PageResult, PageParams } from '@/types/global'
 
 export const getHomeBannerApI = (distributionSite = 1) => {
   return http<BannerItem[]>({
@@ -17,5 +18,26 @@ export const getHomeCategoryAPI = () => {
   return http<CategoryItem[]>({
     method: 'GET',
     url: '/home/category/mutli',
+  })
+}
+// services/home.ts
+/**
+ * 首页-热门推荐-小程序
+ */
+export const getHomeHotAPI = () => {
+  return http<HotItem[]>({
+    method: 'GET',
+    url: '/home/hot/mutli',
+  })
+}
+// src/services/home.ts
+/**
+ * 猜你喜欢-小程序
+ */
+export const getHomeGoodsGuessLikeAPI = (data?: PageParams) => {
+  return http<PageResult<GuessItem>>({
+    method: 'GET',
+    url: '/home/goods/guessLike',
+    data,
   })
 }
