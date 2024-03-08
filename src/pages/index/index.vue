aee634551f3df8b002a3195895f121d5d1f4b767
YXKxk <1843978049@qq.com>
2024-03-08 14:47:40 +0800

    骨架

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,6 +7,7 @@ import { getHomeBannerApI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/
 import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { ref } from 'vue'
 import type { XtxGuessInstance } from '@/types/component'
+import PageSkeleton from './components/PageSkeleton.vue'
 
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
@@ -22,7 +23,7 @@ const getHomeCategoryData = async () => {
 }
 
 const hotList = ref<HotItem[]>([])
-const getHomeHotPanel = async () => {
+const getHomeHotData = async () => {
   const res = await getHomeHotAPI()
   hotList.value = res.result
 }
@@ -33,20 +34,49 @@ const guessRef = ref<XtxGuessInstance>()
 const onScrolltolower = () => {
   guessRef.value?.getMore()
 }
-onLoad(() => {
-  getHomeBannerData()
-  getHomeCategoryData()
-  getHomeHotPanel()
+//
+const isLoading = ref(false)
+
+onLoad(async () => {
+  isLoading.value = true
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+  isLoading.value = false
 })
+//下拉刷新状态
+const isTriggered = ref(false)
+//自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  //开启动画
+  isTriggered.value = true
+  //重置猜你喜欢组件数据
+  guessRef.value?.resetData()
+  await Promise.all([
+    getHomeBannerData(),
+    getHomeCategoryData(),
+    getHomeHotData(),
+    guessRef.value?.getMore(),
+  ])
+  //关闭动画
+  isTriggered.value = false
+}
 </script>
 
 <template>
   <CustomNavbar />
-  <scroll-view scroll-y @scrolltolower="onScrolltolower">
-    <XtxSwiper :list="bannerList" />
-    <CategoryPanel :list="categoryList" />
-    <HotPanel :list="hotList" />
-    <XtxGuess ref="guessRef" />
+  <scroll-view
+    refresher-enabled
+    @refresherrefresh="onRefresherrefresh"
+    :refresher-triggered="isTriggered"
+    scroll-y
+    @scrolltolower="onScrolltolower"
+  >
+    <PageSkeleton v-if="isLoading" />
+    <template v-else>
+      <XtxSwiper :list="bannerList" />
+      <CategoryPanel :list="categoryList" />
+      <HotPanel :list="hotList" />
+      <XtxGuess ref="guessRef" />
+    </template>
   </scroll-view>
 </template>
 
